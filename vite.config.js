1c7f4cf8b77b8cf7971f2b10fa53bb7ebad6845d
Honeylene Insular <honeylene.insular@edu.sait.ca>
2026-02-19 04:41:20 -0700

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { base } from 'motion/react-client'
 
 export default defineConfig({
+  base: '/mu-portfolio-2/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
